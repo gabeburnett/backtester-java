f5db3974a9cdf9efaa768c55b8050408c75f66f0
Gabriel Burnett <gbb37@uclive.ac.nz>
2022-12-27 14:22:37 +1300

    Run docker without TTY

--- example/run.sh
+++ example/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker build -t algo-example .
-docker run --rm -it algo-example:latest ws://host.docker.internal:"$1" "$2"
+docker run --rm -i algo-example:latest ws://host.docker.internal:"$1" "$2"
 docker image rm algo-example
